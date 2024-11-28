46dee4491499f8fe675622043ff25aa1b186d945
RomanGoEmpire <roman.gerloff@icloud.com>
2024-11-28 07:29:44 +0100

    not working group finding

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 import pygame
 import random
 from icecream import ic
@@ -27,14 +28,16 @@ def find_group(x: int, y: int, visited: set = set()):
 
     visited.add((x, y))
 
-    if x > 0 and not x_sequences[x - 1][y]:
-        visited |= find_group(x - 1, y, visited)
     if x < X_AMOUNT - 1 and not x_sequences[x][y]:
-        visited |= find_group(x + 1, y, visited)
-    if y > 0 and not y_sequences[x - 1][y]:
-        visited |= find_group(x, y - 1, visited)
-    if y < Y_AMOUNT - 1 and not y_sequences[x][y]:
-        visited |= find_group(x, y + 1, visited)
+        visited |= find_group(x + 1, y, deepcopy(visited))
+    if y < Y_AMOUNT - 1 and not y_sequences[y][x]:
+        visited |= find_group(x, y + 1, deepcopy(visited))
+
+    if x > 0 and not x_sequences[x - 1][y]:
+        visited |= find_group(x - 1, y, deepcopy(visited))
+    if y > 0 and not y_sequences[y - 1][x]:
+        visited |= find_group(x, y - 1, deepcopy(visited))
+
     return visited
 
 
@@ -62,12 +65,12 @@ while running:
                 reload_sequences()
 
     screen.fill("white")
-    x, y = (0, 0)
-    group = find_group(x, y)
+    # group = find_group(0, 0)
+    # ic(group)
     # for g in group:
     #     x, y = g
     #     pygame.draw.rect(
-    #         screen, "blue", (x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE, GRID_SIZE)
+    #         screen, "gray", (x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE, GRID_SIZE)
     #     )
 
     for x_index, seq in enumerate(y_sequences, 1):
@@ -95,6 +98,6 @@ while running:
     queue = [(x, y) for x in range(X_AMOUNT) for y in range(Y_AMOUNT)]
 
     pygame.display.flip()
-    clock.tick(1)
+    clock.tick(60)
 
 pygame.quit()
